0f57107aa5cf4168975bb9707483252623bafa5d
fots <60556553+fotscode@users.noreply.github.com>
2020-12-19 02:53:50 -0300

    made it prettier

--- youtubeDownloader.py
+++ youtubeDownloader.py
@@ -5,60 +5,67 @@ import pytube
 
 #Main Screen
 master = Tk()
-master.title("Youtube Downloader")
-
-
+master.title("YouTube Downloader")
+master.iconbitmap(r'youtube.ico')
+master.configure(bg="#FFBC42")
 #Default folder
-folder = "C:/Users/cacut/Desktop/projects/youtubeDownloader/videos"
+folder = os.getcwd()
 
 
 #Functions
 
+def downloadVideo(videoUrl):
+    try:
+        yt = pytube.YouTube(videoUrl)# gets video object
+        title = yt.title
+        video = yt.streams.filter(only_audio=True).first()# gets the video file
+        out_file=video.download(folder)
+        new_file=title+".mp3"
+        os.chdir(folder)
+        os.rename(out_file,new_file)# renames file, .mp4 to .mp3
+        notifDownload.config(fg="green",text="Download Completed")# writes in green if succeeded
+    except Exception as e:
+        print(e)
+        notifDownload.config(fg="red",text="Video couldn't be downloaded") # writes in red if failed
+
 def downloadPlaylist():
     playlist_url = url.get()
-    playlist = pytube.contrib.playlist.Playlist(playlist_url)
-    listOfUrls = playlist.video_urls
-    for videoUrl in listOfUrls:
-        try:
-            yt = pytube.YouTube(videoUrl)
-            title = yt.title
-            video = yt.streams.filter(only_audio=True).first()
-            out_file=video.download(folder)
-            
-            new_file=title+".mp3"
-            os.chdir(folder)
-            os.rename(out_file,new_file)
-            notifDownload.config(fg="green",text="Download Completed")
-        except Exception as e:
-            print(e)
-            notifDownload.config(fg="red",text="Video couldn't be downloaded")
+    try:
+        playlist = pytube.contrib.playlist.Playlist(playlist_url) # gets playlist object
+        listOfUrls = playlist.video_urls # makes a list of all the video's urls contained in the playlist
+        for videoUrl in listOfUrls:
+            downloadVideo(videoUrl)
+    except Exception as e:
+        print(e)
+        notifDownload.config(fg="red",text="Link isn't valid")
 
 def searchPath():
     global folder
     folder = filedialog.askdirectory()
-    notifPath.config(fg="green",text=folder)
+    notifPath.config(text=folder)# writes in green the folder directory 
 
 #GUI
 
-Label(master, text="Youtube Converter",fg="red",font=("Calibri",15,"bold")).grid(sticky=N,padx=100,row=0)
+Label(master, text="YouTube Downloader",bg="#FFBC42",fg="red",font=("Calibri",22,"bold")).grid(sticky=N,padx=100,row=0)
 
 #Playlist link
-Label(master,text="Please enter the link to your playlist below:", font=("Calibri",12)).grid(sticky=N,row=1,pady=15)
+Label(master,text="Please enter the link to your playlist below:", bg="#FFBC42",font=("Calibri",12)).grid(sticky=N,row=1,pady=15)
 url = StringVar()
 Entry(master,width=50,textvariable=url).grid(sticky=N,row=2)
 #Button
-Button(master,width=20,text="Download",font=("Calibri",12),command=downloadPlaylist).grid(sticky=N,row=3,pady=15)
+Button(master,width=20,bg="black",fg="white",text="Download Playlist",font=("Calibri",12),command=downloadPlaylist).grid(sticky=N,row=3,pady=15)
+
 
 #Notification Download
-notifDownload = Label(master,font=("Calibri",12))
+notifDownload = Label(master,bg="#FFBC42",font=("Calibri",12,"bold"))
 notifDownload.grid(sticky=N,pady=1,row=4)
 
 #Choose path
-Label(master,text="Directory",font=("Calibri",14)).grid(sticky=N,row=5,pady=15)
-Button(master,width=20,text="Choose path",font=("Calibri",12),command=searchPath).grid(sticky=N,row=6,pady=15)
+Label(master,text="Directory:",bg="#FFBC42",font=("Calibri",14,"bold")).grid(sticky=S,row=5,pady=14)
+Button(master,width=20,bg="black",fg="white",text="Choose path",font=("Calibri",12),command=searchPath).grid(sticky=N,row=6,pady=15)
 
 #Notification Path
-notifPath= Label(master,font=("Calibri",8))
+notifPath= Label(master,text=folder,bg="#FFBC42",fg="black",font=("Calibri",8))
 notifPath.grid(sticky=N,row=7,pady=15)
 
 master.mainloop()
